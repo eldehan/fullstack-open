5ab305aadbee60956b239450fc85523f99bfc24e
Hans Elde <hanserik.elde@gmail.com>
2022-07-12 15:59:55 -0700

    Completed exercise 2.12 (countries step 1)

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,25 +1,76 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-function App() {
+const Search = ({ searchInput, handleSearchInput }) => {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      find countries
+      <input value={searchInput} onChange={handleSearchInput} />
     </div>
   );
-}
+};
+
+const Results = ({ countries }) => {
+  if (Array.isArray(countries)) {
+    if (countries.length > 1) {
+      return (
+        <div>
+          {countries.map((country) => (
+            <p key={country.name.official}>{country.name.common}</p>
+          ))}
+        </div>
+      );
+    } else if (countries.length === 1) {
+      let country = countries[0];
+      let languages = Object.values(country.languages);
+
+      return (
+        <div>
+          <h1>{country.name.common}</h1>
+          <p>capital {country.capital[0]}</p>
+          <p>area {country.area}</p>
+          <h3>Languages:</h3>
+          {languages.map((language) => (
+            <p key={language}>{language}</p>
+          ))}
+        </div>
+      );
+    }
+  } else {
+    return <div></div>;
+  }
+};
+
+const App = () => {
+  const [searchInput, setSearch] = useState("");
+  const [countries, setCountries] = useState([]);
+
+  const handleSearchInput = (event) => {
+    setSearch(event.target.value);
+  };
+
+  const hook = () => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      let results = response.data.filter((country) => {
+        return country.name.common.includes(searchInput);
+      });
+
+      if (results.length > 10) {
+        setCountries("Too many matches, specify another filter");
+      } else {
+        setCountries(results);
+      }
+    });
+  };
+
+  useEffect(hook, [searchInput]);
+
+  return (
+    <>
+      <Search searchInput={searchInput} handleSearchInput={handleSearchInput} />
+      <Results countries={countries} />
+    </>
+  );
+};
 
 export default App;
