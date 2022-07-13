1a0d8f2bc8c528df4cd8eeca08e6f46cd8eb6374
Hans Elde <hanserik.elde@gmail.com>
2022-07-13 10:16:56 -0700

    Revert "completed through exercises 2.14"
    This reverts commit 15b6b91d81b4d0e8f4d493676bb6cf1a3fa80431.

--- part2/countries/src/App.js
+++ part2/countries/src/App.js
@@ -1,47 +1,74 @@
 import { useState, useEffect } from "react";
 import axios from "axios";
 
-import Countries from "./components/Countries";
-import Country from "./components/Country";
+const Search = ({ searchInput, handleSearchInput }) => {
+  return (
+    <div>
+      find countries
+      <input value={searchInput} onChange={handleSearchInput} />
+    </div>
+  );
+};
+
+const Results = ({ countries }) => {
+  if (Array.isArray(countries)) {
+    if (countries.length > 1) {
+      return (
+        <div>
+          {countries.map((country) => (
+            <p key={country.name.official}>{country.name.common}</p>
+          ))}
+        </div>
+      );
+    } else if (countries.length === 1) {
+      let country = countries[0];
+      let languages = Object.values(country.languages);
+
+      return (
+        <div>
+          <h1>{country.name.common}</h1>
+          <p>capital {country.capital[0]}</p>
+          <p>area {country.area}</p>
+          <h3>Languages:</h3>
+          {languages.map((language) => (
+            <p key={language}>{language}</p>
+          ))}
+        </div>
+      );
+    }
+  } else {
+    return <div></div>;
+  }
+};
 
 const App = () => {
-  const [searchInput, setSearchInput] = useState("");
+  const [searchInput, setSearch] = useState("");
   const [countries, setCountries] = useState([]);
-  const [countriesToShow, setCountriesToShow] = useState([]);
 
-  useEffect(() => {
-    axios.get("https://restcountries.com/v3.1/all").then((response) => {
-      setCountries(response.data);
-    });
-  }, []);
-
-  const handleSearchInputChange = (event) => {
-    const input = event.target.value;
-    setSearchInput(input);
-    setCountriesToShow(
-      countries.filter((country) =>
-        country.name.common.toLowerCase().includes(input.toLowerCase())
-      )
-    );
+  const handleSearchInput = (event) => {
+    setSearch(event.target.value);
   };
+
+  const hook = () => {
+    axios.get("https://restcountries.com/v3.1/all").then((response) => {
+      let results = response.data.filter((country) => {
+        return country.name.common.includes(searchInput);
+      });
+
+      if (results.length > 10) {
+        setCountries("Too many matches, specify another filter");
+      } else {
+        setCountries(results);
+      }
+    });
+  };
+
+  useEffect(hook, [searchInput]);
 
   return (
     <>
-      <div>
-        Find countries{" "}
-        <input value={searchInput} onChange={handleSearchInputChange} />
-      </div>
-      {countriesToShow.length === 1 ? (
-        <Country country={countriesToShow[0]} />
-      ) : null}
-      {countriesToShow.length > 10 ? (
-        <div>Too many matches, specify another filter</div>
-      ) : (
-        <Countries
-          countriesToShow={countriesToShow}
-          setCountriesToShow={setCountriesToShow}
-        />
-      )}
+      <Search searchInput={searchInput} handleSearchInput={handleSearchInput} />
+      <Results countries={countries} />
     </>
   );
 };
